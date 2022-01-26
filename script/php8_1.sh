684f07f5702f0477446fe1cd78877bab3f4c7962
shoujii <info@mgoessel.de>
2022-01-26 15:21:57 +0100

    -

--- script/php8_1.sh
+++ script/php8_1.sh
@@ -11,6 +11,9 @@ PHPVERSION8="8.1"
 
 install_packages "php$PHPVERSION8-dev php$PHPVERSION8-fpm php-auth-sasl php$PHPVERSION8-gd php$PHPVERSION8-bcmath php$PHPVERSION8-zip php-mail php-net-dime php-net-url php-pear php-apcu php$PHPVERSION8 php$PHPVERSION8-cli php$PHPVERSION8-common php$PHPVERSION8-curl php$PHPVERSION8-fpm php$PHPVERSION8-intl php$PHPVERSION8-mysql php$PHPVERSION8-soap php$PHPVERSION8-sqlite3 php$PHPVERSION8-xsl php$PHPVERSION8-xmlrpc php-mbstring php-xml php$PHPVERSION8-opcache php$PHPVERSION8-readline php$PHPVERSION8-xml php$PHPVERSION8-mbstring php-memcached php-imagick"
 
+sed_replace_word "memory_limit = 128M" "memory_limit = 512M" "/etc/php/$PHPVERSION8/fpm/php.ini"
+sed_replace_word "upload_max_filesize = 2M" "upload_max_filesize = 512M" "/etc/php/$PHPVERSION8/fpm/php.ini"
+
 systemctl -q restart nginx.service
 systemctl -q restart php$PHPVERSION8-fpm.service
 }
