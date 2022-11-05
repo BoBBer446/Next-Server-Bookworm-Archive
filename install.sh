8c8ac78cf337e2fde2fb9c16232373e4dc3154c6
Marcel Eichlohn <marcel@eichlohn.de>
2022-11-05 13:08:39 +0100

    netcup dns for testing mode

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ if [ -f "$dev_mode" ]; then
     sed_replace_word "ec-384" "ec-384 --staging" "/root/NeXt-Server-Bullseye/script/lets_encrypt.sh"
     sed_replace_word "4096" "4096 --staging" "/root/NeXt-Server-Bullseye/script/mailserver.sh"
     sed_replace_word "4096" "2048" "/root/NeXt-Server-Bullseye/script/dovecot.sh"
+    sed_replace_word "1.1.1.1" "46.38.225.230" "/root/NeXt-Server-Bullseye/script/functions.sh"
     set -x
 fi
 
