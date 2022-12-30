acb191f2517f3bb07ecba921daeca4c0b913a8b7
Marcel Eichlohn <marcel@eichlohn.de>
2022-12-30 14:56:38 +0100

    -

--- script/roundcube.sh
+++ script/roundcube.sh
@@ -7,8 +7,6 @@ trap error_exit ERR
 ROUNDCUBE_USER=$(username)
 ROUNDCUBE_DB_NAME=$(username)
 ROUNDCUBE_DB_PASS=$(password)
-ROUND_HTTPAUTH_USER=$(username)
-ROUND_HTTPAUTH_PASS=$(password)
 RANDOM_DESKEY=$(username)
 
 cd /var/www/${MYDOMAIN}/public/
@@ -27,8 +25,6 @@ mysql -u root -p${MYSQL_ROOT_PASS} -e "FLUSH PRIVILEGES;"
 cd /var/www/${MYDOMAIN}/public/webmail
 mysql -u${ROUNDCUBE_USER} -p${ROUNDCUBE_DB_PASS} -h${MYSQL_HOSTNAME} ${ROUNDCUBE_DB_NAME} < /var/www/${MYDOMAIN}/public/webmail/SQL/mysql.initial.sql
 
-htpasswd -b /etc/nginx/htpasswd/.htpasswd ${ROUND_HTTPAUTH_USER} ${ROUND_HTTPAUTH_PASS}
-
 cp /root/NeXt-Server-Bookworm/configs/roundcube/config.inc.php /var/www/${MYDOMAIN}/public/webmail/config/config.inc.php
 sed_replace_word "rcdbuser" "${ROUNDCUBE_USER}" "/var/www/${MYDOMAIN}/public/webmail/config/config.inc.php"
 sed_replace_word "rcdbpassword" "${ROUNDCUBE_DB_PASS}" "/var/www/${MYDOMAIN}/public/webmail/config/config.inc.php"
@@ -42,16 +38,10 @@ echo "#-------------------------------------------------------------------------
 echo "Roundcube Webmail URL: https://${MYDOMAIN}/webmail/" >> /root/NeXt-Server-Bookworm/login_information.txt
 echo "#------------------------------------------------------------------------------#" >> /root/NeXt-Server-Bookworm/login_information.txt
 echo "#------------------------------------------------------------------------------#" >> /root/NeXt-Server-Bookworm/login_information.txt
-echo "ROUND_HTTPAUTH_USER = ${ROUND_HTTPAUTH_USER}" >> /root/NeXt-Server-Bookworm/login_information.txt
-echo "ROUND_HTTPAUTH_PASS = ${ROUND_HTTPAUTH_PASS}" >> /root/NeXt-Server-Bookworm/login_information.txt
 echo "ROUNDCUBEDBUser = ${ROUNDCUBE_USER}" >> /root/NeXt-Server-Bookworm/login_information.txt
 echo "ROUNDCUBEDBName = ${ROUNDCUBE_DB_NAME}" >> /root/NeXt-Server-Bookworm/login_information.txt
 echo "ROUNDCUBEDBPassword = ${ROUNDCUBE_DB_PASS}" >> /root/NeXt-Server-Bookworm/login_information.txt
 echo "DES_KEY = ${RANDOM_DESKEY}" >> /root/NeXt-Server-Bookworm/login_information.txt
-echo "Disable the http auth?" >> /root/NeXt-Server-Bookworm/login_information.txt
-echo "Use this command and restart the nginx server:" >> /root/NeXt-Server-Bookworm/login_information.txt
-echo "htpasswd -D /etc/nginx/htpasswd/.htpasswd ${ROUND_HTTPAUTH_USER}" >> /root/NeXt-Server-Bookworm/login_information.txt
-echo "systemctl restart nginx.service" >> /root/NeXt-Server-Bookworm/login_information.txt
 echo "#------------------------------------------------------------------------------#" >> /root/NeXt-Server-Bookworm/login_information.txt
 echo "" >> /root/NeXt-Server-Bookworm/login_information.txt
 }
